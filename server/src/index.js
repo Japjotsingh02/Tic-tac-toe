a5fb42a0ca9bdee1e873182b1e7f67ce7c41d482
Japjotsingh02 <sjapjot2002@gmail.com>
2023-01-10 01:25:10 +0530

    App Updated

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,14 @@
 const express=require("express");
 const cors=require("cors");
 const auth=require("./routes/Auth");
-const StreamChat=require("stream-chat");
+const {StreamChat}=require("stream-chat");
 const app=express();
 const router=express.Router();
 
 app.use(cors());
 app.use(express.json());
-const api_key='t2k6kdquctwe';
-const api_secret='utw3pedtxqc6x6utb9qcawmshwgggaw6v2hks4w99um8mz6v43yjumcfr7uge9fe';
+const api_key=process.env.STREAM_CHAT_API_KEY;
+const api_secret=process.env.STREAM_CHAT_API_SECRET;
 const serverClient=StreamChat.getInstance(api_key,api_secret);
 app.use('/auth',auth);
 
